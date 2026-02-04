db535bf8fd5e27ca13ad25ce3f2b320bc5d6f50e
project-faust <pentecost.cody.lane@gmail.com>
2026-02-04 00:43:04 -0500

    updated file structure to incorporate Razor pages; added protection whitelisting root

--- src/BoxOfficeReporting.Api/Program.cs
+++ src/BoxOfficeReporting.Api/Program.cs
@@ -2,12 +2,28 @@ using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
 app.MapGet(
-        "/",
+        "/api",
         () =>
         {
             if (app.Environment.IsDevelopment())
@@ -25,6 +41,7 @@ app.MapGet(
             return Results.Ok(new { service = "BoxOfficeReporting.Api", status = "running" });
         }
     )
+    .AllowAnonymous()
     .WithName("Root");
 
 app.MapGet(
