59015c44d7dea97aff9b547f489280a36a6f31b9
Project-Faust <pentecost.cody.lane@gmail.com>
2026-03-11 13:24:42 -0400

    updated for current report models

--- src/BoxOfficeReporting.Api/Data/ApplicationDbContext.cs
+++ src/BoxOfficeReporting.Api/Data/ApplicationDbContext.cs
@@ -1,15 +1,27 @@
+using BoxOfficeReporting.Api.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using BoxOfficeReporting.Api.Models;
 
 namespace BoxOfficeReporting.Api.Data;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+  {
+  }
 
-    public DbSet<ReportEntry> ReportEntries => Set<ReportEntry>();
+  public DbSet<ReportEntry> ReportEntries => Set<ReportEntry>();
+  public DbSet<ReportEvent> ReportEvents => Set<ReportEvent>();
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+
+    builder.Entity<ReportEntry>()
+      .HasMany(r => r.Events)
+      .WithOne(e => e.ReportEntry)
+      .HasForeignKey(e => e.ReportEntryId)
+      .OnDelete(DeleteBehavior.Cascade);
+  }
 }
