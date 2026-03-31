db15ab1b90f944b1f1fea29681b21f705610b134
Project-Faust <pentecost.cody.lane@gmail.com>
2026-03-31 13:59:08 -0400

    file formatting

--- src/BoxOfficeReporting.Api/Models/ReportEvent.cs
+++ src/BoxOfficeReporting.Api/Models/ReportEvent.cs
@@ -4,17 +4,17 @@ namespace BoxOfficeReporting.Api.Models;
 
 public class ReportEvent
 {
-  public int Id { get; set; }
+    public int Id { get; set; }
 
-  [Required]
-  public int ReportEntryId { get; set; }
+    [Required]
+    public int ReportEntryId { get; set; }
 
-  public ReportEntry ReportEntry { get; set; } = default!;
+    public ReportEntry ReportEntry { get; set; } = default!;
 
-  [Required]
-  [StringLength(200)]
-  public string EventName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(200)]
+    public string EventName { get; set; } = string.Empty;
 
-  [Range(0, 100)]
-  public decimal DeductionPercent { get; set; }
+    [Range(0, 999)]
+    public decimal DeductionPercent { get; set; }
 }
