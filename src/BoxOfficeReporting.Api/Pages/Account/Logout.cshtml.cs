aeae6a2e1cf2768f6a02080810bd8f22c11ee285
Project-Faust <pentecost.cody.lane@gmail.com>
2026-03-31 12:39:09 -0400

    updated local to fix merge

--- src/BoxOfficeReporting.Api/Pages/Account/Logout.cshtml.cs
+++ src/BoxOfficeReporting.Api/Pages/Account/Logout.cshtml.cs
@@ -1,15 +1,15 @@
 using BoxOfficeReporting.Api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RaorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BoxOfficeReporting.Api.Pages.Account;
 
 public class LogoutModel : PageModel
 {
-    private readonly SignInManager<Applicationuser> _signInManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public LogoutModel(SignInManager<Applicationuser> signInManager)
+    public LogoutModel(SignInManager<ApplicationUser> signInManager)
     {
         _signInManager = signInManager;
     }
