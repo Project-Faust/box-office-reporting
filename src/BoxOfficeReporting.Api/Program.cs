d421b6a481c4ac132bd333c31883b32ff2a1e4af
project-faust <pentecost.cody.lane@gmail.com>
2026-04-01 13:26:46 -0400

    updated docker settings

--- src/BoxOfficeReporting.Api/Program.cs
+++ src/BoxOfficeReporting.Api/Program.cs
@@ -1,36 +1,53 @@
+using System.IO;
 using BoxOfficeReporting.Api.Data;
 using BoxOfficeReporting.Api.Models;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
-builder.Services
-    .AddIdentity<ApplicationUser, IdentityRole>(options =>
+var keysPath = builder.Environment.IsDevelopment()
+    ? Path.Combine(builder.Environment.ContentRootPath, "keys")
+    : "/app/keys";
+
+builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(keysPath));
+
+builder
+    .Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     {
         options.SignIn.RequireConfirmedAccount = false;
     })
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services
-    .AddAuthentication()
+builder
+    .Services.AddAuthentication()
     .AddGoogle(options =>
     {
-        options.ClientId = builder.Configuration["Authentication:Google:ClientId"]
+        options.ClientId =
+            builder.Configuration["Authentication:Google:ClientId"]
             ?? throw new InvalidOperationException("Google ClientId is missing.");
 
-        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]
+        options.ClientSecret =
+            builder.Configuration["Authentication:Google:ClientSecret"]
             ?? throw new InvalidOperationException("Google ClientSecret is missing.");
     });
 
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 
